323787b033d62bef3e96cd53c59a96c661a2d086
Lukas van den Dijssel <sydcul@sydcul.com>
2014-10-18 00:53:18 +0200

    Facebook foolproofheidswag
    swag >9000

--- hackathon.sydcul.com/facebook/index.php
+++ hackathon.sydcul.com/facebook/index.php
@@ -1,45 +1,13 @@
 <?php
 require('../libraries/base.inc.php');
 require('../libraries/login.inc.php');
-session_start(); 
-?>
-<!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>Login with Facebook</title>
-<link href="http://www.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"> 
- </head>
-  <body>
-  <?php if ($_SESSION['FBID']): ?>      <!--  After user login  -->
-<div class="container">
-<div class="hero-unit">
-  <h1>Hello <?php echo $_SESSION['USERNAME']; ?></h1>
-  <p>Welcome to "facebook login" tutorial</p>
-  </div>
-<div class="span4">
- <ul class="nav nav-list">
-<li class="nav-header">Image</li>
-	<li><img src="https://graph.facebook.com/<?php echo $_SESSION['USERNAME']; ?>/picture"></li>
-<li class="nav-header">Facebook ID</li>
-<li><?php echo  $_SESSION['FBID']; ?></li>
-<li class="nav-header">Facebook Username</li>
-<li><?php echo $_SESSION['USERNAME']; ?></li>
-<li class="nav-header">Facebook fullname</li>
-<li><?php echo $_SESSION['FULLNAME']; ?></li>
-<div><a href="logout.php">Logout</a></div>
+session_start();
+if(empty($_SESSION['FBID'])) {
+	header('Location: fbconfig.php');
+	die();
+}
 
-<?php
 register($_SESSION['FBID'], 'facebook', null);
+header('Location: http://hackathon.sydcul.com/');
+die();
 ?>
-
-</ul></div></div>
-    <?php else: ?>     <!-- Before login --> 
-<div class="container">
-<h1>Login with Facebook</h1>
-           Not Connected
-<div>
-      <a href="fbconfig.php">Login with Facebook</a></div>
-      </div>
-    <?php endif ?>
-  </body>
-</html>
