9f1410e97febb35def94e5b554eac0a737eafe04
Matthijs Otterloo <matthijsotterloo@Macbook-van-Matthijs.local>
2014-10-18 01:17:32 +0200

    Added level 2

---
+++ hackathon.sydcul.com/level_2.php
@@ -0,0 +1,64 @@
+<html>
+    <head>
+
+        <meta charset="utf-8">
+        <title>Cody | Level 2</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,300,100,100italic,300italic,400italic,700,700italic">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/media-queries.css">
+        <link rel="stylesheet" href="assets/css/bootstrap-social.css">
+        <link href="assets/css/docs.css" rel="stylesheet" >
+ 
+    </head>
+
+<body>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <br>
+                <br>
+                <a href="levels.php"><-- Ga terug naar alle levels</a>
+                <br>    
+                <h1 class="page-header" style="color:#FFFFFF">Level 2
+                    <small style="color:#FFFFFF">Ga op reis met Cody!</small>
+                </h1>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-md-8">
+                <img class="img-responsive" src="http://placehold.it/750x500" alt="">
+            </div>
+
+            <div class="col-md-4">
+                <h3 style="color:#FFFFFF">Cody - Level 1</h3>
+                <p style="color:#FFFFFF">Leuk! Je gaat al beginnen aan level 2! In dit level ga je naar een dorp waar je poppetje langs verschillende huizen gaat.</p>
+                <h3 style="color:#FFFFFF">Wat ga je doen in dit level:</h3>
+                <ul style="color:#FFFFFF">
+                    <li>Gebruik je oude poppetje</li>
+                    <li>Laat je poppetje binear tellen.</li>
+                </ul>
+            </div>
+
+        </div>
+
+        <hr>
+
+        <script src="assets/js/jquery-1.10.2.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script src="assets/js/jquery.ui.map.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+
+</body>
+
+</html>
