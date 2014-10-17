90acf58a14d3f5f69cc8f916bf6ea94aa8b0b661
JoepdeJong <info@joep4u.com>
2014-10-17 23:05:04 +0200

    WIP

---
+++ hackathon.sydcul.com/web/assets/js/zoom.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$(function() {
+		$( "#zoom" ).slider();
+	  });
+  
+$('#workspace').draggable();
+$sliderValue="";
+$("#slider").slider({                   
+                value: 1,
+                min: 0.5,
+                max: 4,
+                step: 0.01,
+                slide: function(event, ui) {
+                                $("#slider_value").val(ui.value);
+                                $("#workspace").animate({ 'zoom': ui.value }, 5);
+                          }
+        });
+        $("#slider_value").val($("#slider").slider("value"));
+});
