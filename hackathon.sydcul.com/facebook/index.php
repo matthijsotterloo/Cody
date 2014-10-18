f3ff8f895cf5dbb882ae0139316812646982aa41
Lukas van den Dijssel <sydcul@sydcul.com>
2014-10-18 02:35:09 +0200

    asd

--- hackathon.sydcul.com/facebook/index.php
+++ hackathon.sydcul.com/facebook/index.php
@@ -8,12 +8,12 @@ if(empty($_SESSION['FBID'])) {
 	header('Location: fbconfig.php');
 	die();
 }
-$usercheck = usercheck($_SESSION['FBID']);
-if(!$usercheck) {
-	register($_SESSION['FBID'], 'facebook', 'facebook');
-} else {
+//$usercheck = usercheck($_SESSION['FBID']);
+//if(empty($usercheck)) {
+//	register($_SESSION['FBID'], 'facebook', 'facebook');
+//} else {
 	login($_SESSION['FBID'], 'facebook');
-}
+//}
 header('Location: http://hackathon.sydcul.com/');
 die();
 ?>
