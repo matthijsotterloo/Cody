d3f20b974494aa91c438d17ac7c2d758c77431fc
Matthijs Otterloo <matthijsotterloo@Macbook-van-Matthijs.local>
2014-10-18 09:33:55 +0200

    Login pagina

--- hackathon.sydcul.com/login.php
+++ hackathon.sydcul.com/login.php
@@ -9,19 +9,25 @@
     <div class="container">
 
       <form class="form-signin" role="form">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="email" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
+        <br>
+        <br>
+        <h2 class="form-signin-heading" style="color:#FFFFFF">Login</h2>
+        <br>
+        <input type="email" class="form-control" placeholder="Email adres" required autofocus>
+        <input type="password" class="form-control" placeholder="Wachtwoord" required>
         <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
+          <input type="checkbox" value="remember-me"> Blijf aangemeld
         </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Aanmelden</button>
       </form>
 
     </div> <!-- /container -->
 
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+            <script src="assets/js/jquery-1.10.2.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script src="assets/js/jquery.ui.map.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
   </body>
 </html>
