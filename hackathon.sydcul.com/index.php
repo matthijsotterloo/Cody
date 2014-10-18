c163999919126c32951f724147c5cf290726088b
Yakim <contact@yakim.nl>
2014-10-18 08:41:28 +0200

    d

--- hackathon.sydcul.com/index.php
+++ hackathon.sydcul.com/index.php
@@ -12,7 +12,7 @@
                     <div class="row">
                         <div class="span12">
                             <div class="logo">
-                                <img src="/branding/logo_home.svg" style="width:600px;height:420px"></img>
+                                <img src="/branding/logo_home.svg" style="width:420px;height:420px"></img>
                             </div>
                             <h2>Hi, ik ben Cody.</h2>
                             <p>
