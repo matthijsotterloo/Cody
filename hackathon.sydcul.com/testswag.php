319754b87b3d099301da5d72b13ea444a3a108a0
Lukas van den Dijssel <sydcul@sydcul.com>
2014-10-18 09:38:06 +0200

    b

--- hackathon.sydcul.com/testswag.php
+++ hackathon.sydcul.com/testswag.php
@@ -5,20 +5,11 @@ include('libraries/login.inc.php');
 <html>
 <body>
 <h1>Inloggen</h1>
-<form action="logintest.php" method="POST">
-<input type="text" name="username"><br>
-<input type="password" name="password"><br>
-<input type="submit"><br>
-</form>
-Ingelogd: <?php
+<?php
 $userid = logincheck();
 var_dump($userid);
 echo('<br>');
 var_dump(getusername($userid));
-?><br>
-<form method="POST" action="logintest.php">
-<input type="hidden" name="logout" value="1">
-<input type="submit" value="Uitloggen">
-</form>
+?>
 </body>
 </html>
