7a819102b9839c97cf353f1db6cf2710f0b9cc68
Matthijs Otterloo <otterloo@server2.local>
2014-10-20 11:11:35 +0200

    Congratulations pagina

---
+++ hackathon.sydcul.com/congratulations.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+
+        <meta charset="utf-8">
+        <title>Cody | Over Cody</title>
+        
+        <?php include 'header.php';?>
+        
+        <div class="coming-soon">
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="span12">
+                            <img src="/branding/Cody.svg"  width="300" height="214"></img>
+                            <h1>Gefeliciteerd!</h1>
+                            <p>
+                              Vanaf nu kan jij ook zelf programmeren!<br>
+                              Wohoooo misschien maak jij later ook wel zo'n toffe website.<br>
+                              <br>
+                              <br>
+                              Wil je nog meer leren? Binnenkort zullen we meer levels toevoegen dus kom binnenkort terug!</p>
+                              <br>
+                              <br>
+                              <a href="http://hackathon.sydcul.com/" target="_blank"><button type="submit" class="btn">&nbsp;Terug naar de website!! &nbsp;</button>
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <script src="assets/js/jquery-1.10.2.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script src="assets/js/jquery.ui.map.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+
+    </body>
+
+</html>
+
